9b6c38a4f9b5bde4e51932fe469df07bc5d65ab3
Armand Gurgu <armandgurgu23@gmail.com>
2020-09-08 21:49:03 -0400

    Added yaml config to define detector config.

---
+++ src/backend/detector/config/detectorDefaults.py
@@ -0,0 +1,23 @@
+from yacs.config import CfgNode as CN
+
+
+class ConfigDefaults(object):
+    def __init__(self):
+        self.defaultConfig = self.makeDefaultConfig()
+
+    def __call__(self, yamlFilePath):
+        self.defaultConfig.merge_from_file(yamlFilePath)
+        self.defaultConfig.freeze()
+        return self.defaultConfig
+
+    def makeDefaultConfig(self):
+        _config = CN()
+        _config.detectorModel = CN()
+        _config.detectorModel.pretrained = True
+        _config.detectorModel.minSize = 100
+        return _config.clone()
+
+
+def getDetectorYamlConfig(yamlFilePath='detector/config/detectorConfig.yaml'):
+    configObject = ConfigDefaults()
+    return configObject(yamlFilePath)
