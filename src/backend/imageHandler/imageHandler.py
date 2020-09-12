3a2afeadd03a30c2ca2b6e48e7667a0441e9f98e
Armand Gurgu <armandgurgu23@gmail.com>
2020-09-11 22:42:54 -0400

    Started implementing static predictions handler.

--- src/backend/imageHandler/imageHandler.py
+++ src/backend/imageHandler/imageHandler.py
@@ -4,6 +4,9 @@
 # compatible with the detector's requirements.
 # (ie: scaling to minimum resolution, normalizing
 # pixel intensity to 0-1 range etc.)
+import os
+import PIL
+from PIL import Image
 
 
 class ImageHandler(object):
@@ -17,7 +20,26 @@ class ImageHandler(object):
             return self.runDynamicPredictionsHandler(dynamicImagePath)
 
     def runStaticPredictionsHandler(self, staticPredictionsPath):
-        pass
+        imagePaths = self.getImageFilenamesFromPath(staticPredictionsPath)
+        imageObjects = self.openImagesFromImageFiles(imagePaths, staticPredictionsPath)
+        raise NotImplementedError('Finish code for image handler here!')
+
+    def openImagesFromImageFiles(self, imagePaths, staticPredictionsPath):
+        imageObjects = []
+        for currImagePath in imagePaths:
+            fullImagePath = os.path.join(staticPredictionsPath, currImagePath)
+            currImageObject = Image.open(fullImagePath)
+            imageObjects.append(currImageObject)
+        return imageObjects
+
+    def getImageFilenamesFromPath(self, staticPredictionsPath):
+        currPath = os.getcwd()
+        os.chdir(staticPredictionsPath)
+        allFiles = os.listdir(os.getcwd())
+        assert '.DS_Store' not in allFiles, 'Found .DS_Store in list of image files! {}'.format(
+            allFiles)
+        os.chdir(currPath)
+        return allFiles
 
     def runDynamicPredictionsHandler(self, dynamicImagePath):
         raise NotImplementedError(
