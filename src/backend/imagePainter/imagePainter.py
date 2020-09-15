441592c1d14210bd809434612ff1254137d55f6d
Armand Gurgu <armandgurgu23@gmail.com>
2020-09-14 22:51:57 -0400

    Added logic for color choice loading and sampling.

--- src/backend/imagePainter/imagePainter.py
+++ src/backend/imagePainter/imagePainter.py
@@ -1,26 +1,51 @@
 import PIL
 from PIL import ImageDraw
+import random
 
 
 class ImagePainter(object):
-    def __init__(self, boxWidth, textPixelShift, textStrokeWidth, colorChoice):
+    def __init__(self, boxWidth, textPixelShift, textStrokeWidth, colorChoice, colorFilePath):
         self.boxWidth = boxWidth
         self.textPixelShift = textPixelShift
         self.textStrokeWidth = textStrokeWidth
         self.colorChoice = colorChoice
+        self.colorFilePath = colorFilePath
+        # Open the list of colors that can be sampled for drawing.
         if self.colorChoice == 'random':
             # Fetch a color sampling method.
+            self.colorSpace = self.openColorSpaceFromFile(self.colorFilePath)
             self.colorSampler = self.sampleColorFromColorSpace
         else:
             # We assign a uniform color to
             # be used for drawing.
-            self.drawColor = self.colorChoice
+            self.colorSpace = self.colorChoice
 
     def __call__(self, imageList, predictionData):
+        self.drawPredictionsOnImages(imageList, predictionData)
+        return
+
+    def drawPredictionsOnImages(self, imageList, predictionData):
+        for imageIndex, currImage in imageList:
+            currDrawColor = self.colorSampler()
+            currCanvas = ImageDraw.Draw()
+            # To Do: Complete logic in this function.
+        pass
+
+    def drawBoxesOnCanvas(self, currCanvas, predictedBoxes):
         pass
 
     def sampleColorFromColorSpace(self):
-        pass
+        return random.choice(self.colorSpace)
 
-    def openColorSpaceFromFile(self):
-        pass
+    def openColorSpaceFromFile(self, colorPath):
+        allColors = []
+        with open(colorPath, 'r') as colorFile:
+            for currColor in colorFile:
+                currColor = currColor.strip('\n')
+                allColors.append(currColor)
+        return allColors
+
+    def showImages(self, imageList):
+        for currImage in imageList:
+            currImage.show()
+        return
