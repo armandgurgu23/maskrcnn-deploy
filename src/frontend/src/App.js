92b0af88966d230e6cadc0f3014941c96dad0c65
Armand Gurgu <armandgurgu23@gmail.com>
2020-10-17 08:33:25 -0400

    Finished basic fast-api backend. Starting frontend.

---
+++ src/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Welcome to the Mask-RCNN demo!!</h1>
+    </div>
+  );
+}
+
+export default App;
