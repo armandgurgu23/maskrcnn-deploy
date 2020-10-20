e36d05089fc71b5ee14e4e3d8f87f231d7857818
Armand Gurgu <armandgurgu23@gmail.com>
2020-10-20 08:04:23 -0400

    Implemented upload button and file extraction.

--- src/frontend/src/components/uploadButton.js
+++ src/frontend/src/components/uploadButton.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import './uploadButton.css';
 class UploadButton extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {uploadPath:""};
+    }
+
+
+    handleInputChange = (event) => {
+        let fileUploaded = event.target.files;
+        this.setState({uploadPath:fileUploaded[0]});
+        console.log('this.state after setState = ', this.state)
+    }
+
+
+    handleButtonClick = () => {
+        console.log('Inside handleButtonClick! state = ', this.state);
+        let inputElem = document.getElementById("uploadInput");
+        inputElem.click();
+    }
+
+    //Note: add the multiple attribute inside the uploadInput container to allow for
+    //multiple file submissions!
     render() {
         return (
         <div id='Upload Image Container'>
-            <label for="upload"> Upload an image </label>
-            <input id="upload" type="file" accept='image/png,image/jpeg,image/jpg'></input>
+            <input id="uploadInput" type="file" accept='image/png,image/jpeg,image/jpg' onChange={this.handleInputChange}></input>
+            <button id="uploadButton" onClick={this.handleButtonClick}>Upload Image</button>
         </div>);
     }
 }
