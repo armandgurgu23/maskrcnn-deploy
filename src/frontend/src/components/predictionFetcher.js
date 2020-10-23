ef985a52ce047a55f4291e910a9cdd37fb2ab49d
Armand Gurgu <armandgurgu23@gmail.com>
2020-10-23 07:54:57 -0400

    Added axios as dependency for REST Api handling.

--- src/frontend/src/components/predictionFetcher.js
+++ src/frontend/src/components/predictionFetcher.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import './predictionFetcher.css';
 
 class PredictionFetcher extends React.Component {
@@ -9,6 +10,7 @@ class PredictionFetcher extends React.Component {
 
     renderPredictionButton = () => {
         console.log('Props passed to Prediction Fetcher: ', this.props);
+        console.log('What is axios: ', axios);
         return <button id='predictionButton'>Run Object Detector</button>;
     }
 
