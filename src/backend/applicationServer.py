7a39ba6743987b6915af758158575b76bfbff5ac
Armand Gurgu <armandgurgu23@gmail.com>
2020-11-30 23:22:34 -0500

    Refactoring web server (fast-api) code in preparation for docker deployment.

--- src/backend/applicationServer.py
+++ src/backend/applicationServer.py
@@ -4,9 +4,10 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse, StreamingResponse, RedirectResponse
 from modelServer import ModelServer
 import logging
+import uvicorn
+import argparse
 # Create the FastAPI application server.
 server = FastAPI()
-modelService = ModelServer()
 
 
 def getApplicationLogger():
@@ -15,8 +16,6 @@ def getApplicationLogger():
     # server.
     return logging.getLogger("uvicorn.info")
 
-
-applicationLogger = getApplicationLogger()
 
 # A list of all the front-end origins that are allowed to access the server.
 # We choose them depending on the development mode flag:
@@ -48,9 +47,6 @@ def setupCORSMiddleware(server):
                           allow_methods=["*"],
                           allow_headers=["*"])
     return
-
-
-setupCORSMiddleware(server)
 
 
 @server.get('/healthcheck')
@@ -95,3 +91,22 @@ def runObjectSegmentor(imageFile: UploadFile = File(...)):
     predictionsImage = modelService(
         imageFile.file, imageExtension, 'segmentor')
     return StreamingResponse(predictionsImage, media_type=imageFile.content_type)
+
+
+def getApplicationStartUpArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--host", type=str, help="The URL of the application host. Defaults to localhost.", default="localhost")
+    parser.add_argument(
+        "--port", type=int, help="The host server machine port to run the application on.", default=6969)
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    modelService = ModelServer()
+    applicationLogger = getApplicationLogger()
+    setupCORSMiddleware(server)
+    applicationArgs = getApplicationStartUpArgs()
+    uvicorn.run("applicationServer:server", host=applicationArgs.host,
+                port=applicationArgs.port)
